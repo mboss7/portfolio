79225a910f30f8709699ec2428105516357cc80b
mboss7 <102410364+mboss7@users.noreply.github.com>
2023-03-21 19:57:25 +0100

    maj h1

--- mboss-portfolio/src/App.tsx
+++ mboss-portfolio/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + TEST_DEPLOY</h1>
+      <h1>PortFolio of M4tt</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
