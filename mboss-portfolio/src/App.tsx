e88d131165a6a2b580133ab34b8c340ae0ba5bd6
mboss7 <102410364+mboss7@users.noreply.github.com>
2023-03-21 20:33:55 +0100

    maj

--- mboss-portfolio/src/App.tsx
+++ mboss-portfolio/src/App.tsx
@@ -9,24 +9,15 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>PortFolio of M4tt</h1>
+      <h1>M4tt Portfolio</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+           Welcome in my portfolio page ! 
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Made with passion by M4tt 
       </p>
     </div>
   )
